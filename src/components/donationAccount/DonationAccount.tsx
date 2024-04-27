bf8471d9224e8e81edd0c47c1501632710d68525
yonainthefish <yonaisgood@gmail.com>
2024-04-27 22:58:26 +0900

    Design : 수정요청사항에 맞춰 디자인 수정

--- src/components/donationAccount/DonationAccount.tsx
+++ src/components/donationAccount/DonationAccount.tsx
@@ -1,30 +1,38 @@
+import { useState } from 'react';
 import { ButtonModal } from './DonationAccountStyle';
 import DonationBackgound from '../../assets/bankAccountBack.png';
 
 export default function DonationAccount() {
   const accountNumber = '207013-52-104247';
+  const accountOwner = '농협 예금주: 최병한(장로회 회장)';
+  const [buttonText, setButtonText] = useState('복사');
 
   const copyToClipboard = (text: string) => {
-    navigator.clipboard.writeText(text);
+    navigator.clipboard.writeText(text).then(() => {
+      setButtonText('완료');
+      setTimeout(() => {
+        setButtonText('복사');
+      }, 2000);
+    });
   };
 
   return (
     <ButtonModal>
-      <div>
+      <div className="DonationFlower">
+        <img src={DonationBackgound} alt="" />
+      </div>
+      <div className="DonationNumber">
         <h3>후원 계좌</h3>
         <div className="bankNumber">
-          <p className='accountNum'>{accountNumber}</p>
+          <p className="accountNum">{accountNumber}</p>
           <button
             className="copy"
             onClick={() => copyToClipboard(accountNumber)}
           >
-            복사하기
+            {buttonText}
           </button>
         </div>
-        <p>농협 예금주: 최병한(장로회 회장)</p>
-      </div>
-      <div className="DonationFlower">
-        <img src={DonationBackgound} alt="" />
+        <p>{accountOwner}</p>
       </div>
     </ButtonModal>
   );
--- src/components/donationAccount/DonationAccountStyle.ts
+++ src/components/donationAccount/DonationAccountStyle.ts
@@ -2,12 +2,51 @@ import styled from 'styled-components';
 
 const ButtonModal = styled.div`
   width: 100%;
+  height: 200px;
   position: relative;
   padding: 10px;
   border-radius: 10px;
-
   text-align: center;
   margin-top: 100px;
+
+  .DonationNumber {
+    width: 100%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    h3 {
+      margin-bottom: 10px;
+    }
+
+    .bankNumber {
+      width: 60%;
+      margin: 0 auto;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    .accountNum {
+      font-size: 18px;
+      font-weight: 900;
+    }
+
+    .copy {
+      padding: 0 7px;
+      border-radius: 5px;
+      background-color: var(--gray-100);
+      color: var(--white-900);
+      font-size: 15px;
+    }
+
+    .close {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+    }
+  }
 
   .DonationFlower {
     position: absolute;
@@ -21,37 +60,6 @@ const ButtonModal = styled.div`
       padding: 0;
       opacity: 30%;
     }
-  }
-
-  h3 {
-    margin-bottom: 10px;
-  }
-
-  .bankNumber {
-    width: 65%;
-    margin: 0 auto;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  .accountNum {
-    font-size: 18px;
-    font-weight: 900;
-  }
-
-  .copy {
-    padding: 0 7px;
-    border-radius: 5px;
-    background-color: var(--gray-100);
-    color: var(--white-900);
-    font-size: 15px;
-  }
-
-  .close {
-    position: absolute;
-    top: 10px;
-    right: 10px;
   }
 `;
 
